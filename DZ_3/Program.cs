4f3b1dd9a01d199d5f90f39e90f47b3cc314ea71
PowerLay <34715449+PowerLay@users.noreply.github.com>
2020-11-06 16:03:38 +0300

    Add Subtraction

--- DZ_3/Program.cs
+++ DZ_3/Program.cs
@@ -6,10 +6,10 @@ namespace DZ_3
 {
     internal class Program
     {
-        //\u2229-Пересечение
-        //\u22C3-Объединение
+        //^-Пересечение
+        //v-Объединение
         public static string Text =
-            "A={b,e,f,k,t};B={f,i,j,p,y};C={j,k,l,y};D={i,j,s,t,u,y,z};X={(A\u2229C)\u22C3(-B\u2229C)}";
+            "A={b,e,f,k,t};B={f,i,j,p,y};C={j,k,l,y};D={i,j,s,t,u,y,z};X={(A^C)v(B^C)};Y={(A^-B)v(D\\C)}";
 
         public static List<char> A = new List<char>();
         public static List<char> B = new List<char>();
@@ -17,8 +17,7 @@ namespace DZ_3
         public static List<char> D = new List<char>();
         public static List<char> U = new List<char>();
         public static List<char> X = new List<char>();
-        private static readonly int TextLength = Text.Length;
-
+        public static List<char> Y = new List<char>();
 
         private static void Main(string[] args)
         {
@@ -32,16 +31,21 @@ namespace DZ_3
             foreach (var c in C) Console.WriteLine(c);
             Console.WriteLine("This is D:");
             foreach (var c in D) Console.WriteLine(c);
+            Console.WriteLine("This is U:");
+            foreach (var c in U) Console.WriteLine(c);
+            Console.WriteLine("\nThis is X:");
+            foreach (var c in X) Console.WriteLine(c);
+            Console.WriteLine("\nThis is Y:");
+            foreach (var c in Y) Console.WriteLine(c);
         }
 
         public static List<char> Union(List<char> list1, List<char> list2)
         {
-            var Temp = new List<char>(list1);
-            Temp.AddRange(list2);
-            Temp = Temp.Distinct().ToList();
-            return Temp;
+            var temp = new List<char>(list1);
+            temp.AddRange(list2);
+            temp = temp.Distinct().ToList();
+            return temp;
         }
-
         public static List<char> Crossing(List<char> list1, List<char> list2)
         {
             return list1.Where(list2.Contains).ToList();
@@ -49,119 +53,62 @@ namespace DZ_3
 
         public static List<char> Negation(List<char> list) //Не чистит за собой Temp
         {
-            var Temp = new List<char>(U);
-            foreach (var t in list) Temp.Remove(t);
+            var temp = new List<char>(U);
+            foreach (var t in list) temp.Remove(t);
 
-            return Temp;
+            return temp;
         }
+
+        public static List<char> Subtraction(List<char> list1, List<char> list2)
+        {
+            var temp = new List<char>(list1);
+            foreach (var t in list2) temp.Remove(t);
+            return temp;
+        }
+
 
         public static void FillArrayList(string text)
         {
             var textArr = text.Split(';');
             int startIndex;
-            for (int i = 0; i < 4; i++)
+            for (var i = 0; i < 4; i++)
             {
                 var tempStrArr = textArr[i].Split('=');
                 startIndex = tempStrArr[1].IndexOf('{');
                 var values = tempStrArr[1].Substring(startIndex + 1, tempStrArr[1].Length - startIndex - 2);
-                var arr = values.Replace(",","").ToCharArray();
+                var arr = values.Replace(",", "").ToCharArray();
 
                 switch (tempStrArr[0][0])
                 {
                     case 'A':
                         A = new List<char>(arr);
+                        A.Sort();
                         break;
                     case 'B':
                         B = new List<char>(arr);
+                        B.Sort();
                         break;
                     case 'C':
                         C = new List<char>(arr);
+                        C.Sort();
                         break;
                     case 'D':
                         D = new List<char>(arr);
+                        D.Sort();
                         break;
                 }
-            }
-            startIndex = textArr[4].IndexOf('{');
-            X = Calc(textArr[4].Substring(startIndex + 1, textArr[4].Length - startIndex - 2));
-        }
-        public static void CreateArrayList()
-        {
-            var vrm = 0;
-
-            bool exitFlag;
-            for (var i = 0; i < TextLength; i++)
-            {
-                var temp = new List<char>();
-                switch (Text[i])
-                {
-                    case 'A':
-                        temp = A;
-                        break;
-                    case 'B':
-                        temp = B;
-                        break;
-                    case 'C':
-                        temp = C;
-                        break;
-                    case 'D':
-                        temp = D;
-                        break;
-                    case 'X':
-                        vrm = i;
-                        i = TextLength;
-                        break;
-                }
-
-                exitFlag = false;
-                for (var j = i + 1; j < TextLength; j++)
-                    if (!exitFlag)
-                        switch (Text[j])
-                        {
-                            case '=':
-                            case ',':
-                            case '{':
-                            case '}':
-                                continue;
-                            case ';':
-                                i = j;
-                                exitFlag = true;
-                                break;
-                            default:
-                                temp.Add(Text[j]);
-                                break;
-                        }
-                    else
-                        break;
             }
 
             U = Union(Union(A, B), Union(C, D));
+            U.Sort();
 
-            Console.WriteLine("This is U:");
-            foreach (var c in U) Console.WriteLine(c);
+            startIndex = textArr[4].IndexOf('{');
+            X = Calc(textArr[4].Substring(startIndex + 1, textArr[4].Length - startIndex - 2));
+            X.Sort();
 
-            var subStr = "";
-            var read = false;
-            for (var i = vrm; i < TextLength; i++)
-            {
-                if (Text[i] == '{')
-                {
-                    read = true;
-                    continue;
-                }
-
-                if (Text[i] == '}')
-                {
-                    read = false;
-                    break;
-                }
-
-                if (read) subStr += Text[i];
-            }
-
-            Console.WriteLine("This is X:");
-            var res = Calc(subStr);
-            foreach (var c in res) Console.WriteLine(c);
+            startIndex = textArr[5].IndexOf('{');
+            Y = Calc(textArr[5].Substring(startIndex + 1, textArr[5].Length - startIndex - 2));
+            Y.Sort();
         }
 
         private static List<char> Calc(string expression)
@@ -182,6 +129,7 @@ namespace DZ_3
                     else
                     {
                         temp = Negation(Calc(expression[i + 1]));
+                        i++;
                     }
                 }
                 else if (c == '(')
@@ -189,21 +137,7 @@ namespace DZ_3
                     var tempExpression = GetExpression(expression, ref i);
                     temp = Calc(tempExpression);
                 }
-                else if (c == '\u22C3')
-                {
-                    if (expression[i + 1] == '(')
-                    {
-                        i++;
-                        var tempExpression = GetExpression(expression, ref i);
-                        temp = Union(temp, Calc(tempExpression));
-                    }
-                    else
-                    {
-                        temp = Union(temp, Calc(expression[i + 1]));
-                        i++;
-                    }
-                }
-                else if (c == '\u2229')
+                else if (c == '^')
                 {
                     if (expression[i + 1] == '(')
                     {
@@ -211,9 +145,47 @@ namespace DZ_3
                         var tempExpression = GetExpression(expression, ref i);
                         temp = Crossing(temp, Calc(tempExpression));
                     }
+                    else if (expression[i + 1] == '-')
+                    {
+                        temp = Crossing(temp, Calc(expression.Substring(i + 1, 2)));
+                        i += 2;
+                    }
                     else
                     {
                         temp = Crossing(temp, Calc(expression[i + 1]));
+                        i++;
+                    }
+                }
+                else if (c == 'v')
+                {
+                    if (expression[i + 1] == '(')
+                    {
+                        i++;
+                        var tempExpression = GetExpression(expression, ref i);
+                        temp = Union(temp, Calc(tempExpression));
+                    }
+                    else if (expression[i + 1] == '-')
+                    {
+                        temp = Union(temp, Calc(expression.Substring(i+1,2)));
+                        i+=2;
+                    }
+                    else
+                    {
+                        temp = Union(temp, Calc(expression[i + 1]));
+                        i++;
+                    }
+                }
+                else if (c == '\\')
+                {
+                    if (expression[i + 1] == '(')
+                    {
+                        i++;
+                        var tempExpression = GetExpression(expression, ref i);
+                        temp = Subtraction(temp, Calc(tempExpression));
+                    }
+                    else
+                    {
+                        temp = Subtraction(temp, Calc(expression[i + 1]));
                         i++;
                     }
                 }
